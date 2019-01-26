ace38cf521c07d65039392f52d5c49f563e44adc
Biome21 <guillaume.asselin@icloud.com>
2019-01-26 12:11:15 -0500

    Finishing planctonSpawner

---
+++ MainProject/Assets/Scripts/Environment/Planction/Plancton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Plancton : MonoBehaviour {
+
+	public Action<Plancton> m_OnPlanctonDied;
+
+	[Header("Life Values")]
+	public float m_MinPlanctionLifetime = 5f;
+	public float m_MaxPlanctonLifetime = 10f;
+
+	private float m_LifetimeTimer;
+	private bool m_IsAlive = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+	{
+		if (m_IsAlive)
+		{
+			m_LifetimeTimer -= Time.deltaTime;
+
+			if (m_LifetimeTimer <= 0)
+			{
+				Die();
+			}
+		}
+	}
+
+	public void Spawn()
+	{
+		m_LifetimeTimer = UnityEngine.Random.Range (m_MinPlanctionLifetime, m_MaxPlanctonLifetime);
+		m_IsAlive = true;
+		gameObject.SetActive(true);
+	}
+
+	public void Die()
+	{
+		m_IsAlive = false;
+		gameObject.SetActive(false);
+		m_OnPlanctonDied (this);
+	}
+}
--- MainProject/Assets/Scripts/Environment/Planction/PlanctonSpawner.cs
+++ MainProject/Assets/Scripts/Environment/Planction/PlanctonSpawner.cs
@@ -8,29 +8,72 @@ public class PlanctonSpawner : MonoBehaviour {
 	[Header("Spawn Values")]
 	public int m_MinPlanctonOnScreen = 5;
 	public int m_MaxPlanctonOnScreen = 10;
-	public float m_MinPlanctionLifetime = 5f;
-	public float m_MaxPlanctonLifetime = 10f;
-	public float m_CameraMarginX = 50f;
-	public float m_CameraMarginY = 50f;
+	public float m_MinRefreshTime = 2;
+	public float m_MaxRefreshTime = 5;
 	public float m_PlanctonSize;
 
 	public GameObject m_PlanctonPrefab;
 
+	private int m_PlanctonOnScreen = 0;
+	private List<Plancton> m_UnusedPlancton = new List<Plancton>();
+	private List<Plancton> m_UsedPlancton = new List<Plancton>();
+
+	private float m_RefreshTime = 0;
+	private bool m_ShouldSpawn = false;
+
 	// Use this for initialization
 	private void Start ()
 	{
+		Initialize ();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+	{
+		if (m_ShouldSpawn)
+		{
+			m_RefreshTime -= Time.deltaTime;
+
+			if (m_RefreshTime <= 0 || m_PlanctonOnScreen < m_MinPlanctonOnScreen) {
+				int numberOfPlanctonToSpawn = UnityEngine.Random.Range (Mathf.Max (0, m_MinPlanctonOnScreen - m_PlanctonOnScreen), m_MaxPlanctonOnScreen - m_PlanctonOnScreen + 1);
+				for (int i = 0; i < numberOfPlanctonToSpawn; i++) {
+					Spawn ();
+				}
+
+				if (m_RefreshTime <= 0) {
+					m_RefreshTime = UnityEngine.Random.Range (m_MinRefreshTime, m_MaxRefreshTime);				
+				}
+			}
+		}
+	}
+
+	public void Initialize()
+	{
 		//Spawn initial plancton on screen.
-		int startNumber = UnityEngine.Random.Range(m_MinPlanctonOnScreen, m_MaxPlanctonOnScreen);
+		int startNumber = UnityEngine.Random.Range(m_MinPlanctonOnScreen, m_MaxPlanctonOnScreen + 1);
 
 		for (int i = 0; i < startNumber; i++)
 		{
 			Spawn ();
 		}
+
+		m_RefreshTime = UnityEngine.Random.Range (m_MinRefreshTime, m_MaxRefreshTime);
+
+		m_ShouldSpawn = true;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void StopSpawning()
+	{
+		m_ShouldSpawn = false;
+	}
+
+	public void Reset()
+	{
+		StopSpawning();
+		for (int i = 0; i < m_UsedPlancton.Count; i++)
+		{
+			m_UsedPlancton [i].Die();
+		}
 	}
 
 	//Private
@@ -47,9 +90,33 @@ public class PlanctonSpawner : MonoBehaviour {
 				UnityEngine.Random.Range (-halfCameraHeight + m_PlanctonSize, halfCameraHeight - m_PlanctonSize));
 
 			hits = Physics2D.OverlapCircleAll (planctonPosition, m_PlanctonSize, 1 << LayerMask.NameToLayer("Plancton"));
-			Debug.Log(hits.Length);
 		} while(hits.Length > 0);
 
-		Instantiate(m_PlanctonPrefab, planctonPosition, Quaternion.identity, transform);
+		Plancton plancton;
+		if (m_UnusedPlancton.Count > 0)
+		{
+			plancton = m_UnusedPlancton [0];
+			m_UnusedPlancton.RemoveAt (0);
+		}
+		else
+		{
+			GameObject planctonObj = Instantiate (m_PlanctonPrefab, planctonPosition, Quaternion.identity, transform);
+			plancton = planctonObj.GetComponent<Plancton> ();
+		}
+
+		plancton.m_OnPlanctonDied += OnPlanctonDied;
+		plancton.Spawn();
+
+		m_UsedPlancton.Add (plancton);
+		m_PlanctonOnScreen++;
+	}
+
+	private void OnPlanctonDied(Plancton plancton)
+	{
+		plancton.m_OnPlanctonDied -= OnPlanctonDied;
+		m_PlanctonOnScreen--;
+
+		m_UsedPlancton.Remove (plancton);
+		m_UnusedPlancton.Add (plancton);
 	}
 }
